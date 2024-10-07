c64b033887b26a3acef7b91c7852814294200e97
JBufl <jb2ndr@ufl.edu>
2024-10-07 11:03:08 -0400

    Manuscript Data

---
+++ 2024_01_10_NVIDIA RTX 3500 Ada Generation Laptop GPU/_delbin.bat
@@ -0,0 +1,4 @@
+del /Q J:\FPIBGPDATAManuscript\perfdataPQB\*.bin
+del /Q J:\FPIBGPDATAManuscript\perfdataDUP\*.bin
+del /Q J:\FPIBGPDATAManuscript\perfdataPCD\*.bin
+del /Q J:\FPIBGPDATAManuscript\perfdataCFB\*.bin
